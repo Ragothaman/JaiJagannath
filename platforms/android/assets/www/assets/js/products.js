c5b7341bf32f36c57086ac64c192bfad15a6458d
Ragothaman <ragothaman.p@gmail.com>
2017-04-22 19:55:59 -0400

    Final Project

---
+++ platforms/android/assets/www/assets/js/products.js
@@ -0,0 +1,103 @@
+/**
+ * Product fetching functions
+ */
+
+var productData;
+var perishableData;
+var nonPerishableData;
+
+$(document).ready(function(){
+	loadProducts();
+});
+
+function generateTabContent(content, isPerish) {
+	var productName = '';
+	var tag = '<div class="tiles-cont">';
+	for (var i = 0; i < content.length; i++) {
+		productName = content[i].productname;
+		console.log("Product Name : "+content[i].productname);
+		if (i % 2 == 0) {
+			tag += '<div class="sub-tiles" id="test-sub"><div class="row"><div class="col-xs-6 m_both"><div class="tile tile-small bg-laddu disp-table" style="background:url('
+					+ content[i].imagename
+					+ ') center center no-repeat" ';
+			if(isPerish)
+				tag += 'data-sub-tile';
+			else
+				tag += 'data-sub-tile-non';
+			tag += '="'+i+'" data-toggle="modal" data-target="#infomodal"><h3 class="title table-cell vert-middle"> '
+					+ productName
+					+ ' </h3><div class="price-cart"><span class="amount"> $ '
+					+ content[i].price
+					+ ' </span><a class="btn btn-info cart-btn" data-add-cart="'
+					+ i
+					+ '"> ADD TO CART </a><input type="hidden" id="isPerish" value="'
+					+ content[i].perishable + '"></div></div></div>';
+		} else {
+			tag += '<div class="col-xs-6 m_both"><div class="tile tile-small bg-laddu disp-table" style="background:url('
+					+ content[i].imagename
+					+ ') center center no-repeat" '
+			if(isPerish)
+				tag += 'data-sub-tile';
+			else
+				tag += 'data-sub-tile-non';
+			tag += '="'+i+'" data-toggle="modal" data-target="#infomodal"><h3 class="title table-cell vert-middle"> '
+					+ productName
+					+ ' </h3><div class="price-cart"><span class="amount"> $ '
+					+ content[i].price
+					+ ' </span><a class="btn btn-info cart-btn" data-add-cart="'
+					+ i
+					+ '"> ADD TO CART </a><input type="hidden" id="isPerish" value="'
+					+ content[i].perishable + '"></div></div></div>';
+		}
+		if (i % 2 != 0) {
+			tag += '</div></div>';
+		}
+	}
+	tag += '</div>';
+	if(isPerish) {
+		//console.log("Product Items "+tag);
+		$(tag).insertAfter($("#perishTiles .row"));
+	} else {
+		//console.log("No Perishable Product Items "+tag);
+		$(tag).insertAfter($("#nonPerishTiles .row"));
+	}
+}
+
+function loadProducts() {
+	// 'http://ec2-54-187-15-155.us-west-2.compute.amazonaws.com:8080/PoCAppServer/products/getperishableproducts'//
+	//http://http://ec2-54-71-129-64.us-west-2.compute.amazonaws.com:8080/PoCAppServer/products/getallproducts//
+	$(function() {
+		$.ajax({
+			type : 'GET',
+			contentType : 'application/html',
+			url : 'http://ec2-54-187-15-155.us-west-2.compute.amazonaws.com:8080/PoCAppServer/products/getallproducts',
+			success : function(data, textStatus, jqXHR) {
+				if(data) {
+					localStorage.setItem("productData", data);
+					var perish = new Array();
+					var nonPerish = new Array();
+					var j = 0;
+					var k = 0;
+					for (var i = 0; i < data.length; i++) {
+						if (data[i].perishable == 'true') {
+							perish[j] = data[i];
+							j++;
+						} else {
+							nonPerish[k] = data[i];
+							k++;
+						}
+					}
+					perishableData = perish;
+					nonPerishableData = nonPerish;
+					generateTabContent(perish, true);
+					generateTabContent(nonPerish, false);
+					productData = data;
+				}
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				var err = eval("(" + jqXHR.responseText + ")");
+			}
+
+		});
+	});
+}
---
+++ www/assets/js/custom.js
@@ -0,0 +1,221 @@
+var itemArrObj=[];
+$(document).ready(function(){
+    // toggle on left side
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").addClass("toggled");
+    }).blur(function(e){
+        e.preventDefault();
+        $("#wrapper").removeClass("toggled");
+    });
+
+
+    //tab content based on button click
+    $(document).off('click','.menucont [data-tab-btn]');
+    $(document).on('click','.menucont [data-tab-btn]', function(){
+        $('[data-tab-btn]').removeClass('active')
+        $(this).addClass('active');
+        var clicked = $(this).attr('data-tab-btn');
+        $('.tabcontent [data-tab-content]').fadeOut();
+        $('.tabcontent [data-tab-content="'+clicked+'"]').fadeIn();
+    });
+
+    //tab content based on button click
+    $(document).off('click','.optioncont [data-tab-btn]');
+    $(document).on('click','.optioncont [data-tab-btn]', function(){
+        $('[data-tab-btn]').removeClass('active')
+        $(this).addClass('active');
+        var clicked = $(this).attr('data-tab-btn');
+        $('.tabcontent [data-tab-content]').hide();
+        $('.tabcontent [data-tab-content="'+clicked+'"]').show();
+    });
+
+    $(document).off('click', '[data-main-tile]');
+    $(document).on('click', '[data-main-tile]', function(){
+        var temp = $(this).attr('data-main-tile');
+        pageRedirect(temp);
+    });
+
+    // $(document).off('click', '[data-sub-tile]');
+    // $(document).on('click', '[data-sub-tile]', function(){
+    //     var temp = $(this).attr('data-sub-tile');
+    // });
+
+    //add cart button from tile itself
+    $(document).off('click', '[data-add-cart]')
+    $(document).on('click', '[data-add-cart]', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        var tabName = $("[data-tab-btn].active").attr('data-tab-btn');
+        var cartId = $(this).attr('data-add-cart');
+        var cartData;
+        if(tabName == 'Perishable') {
+        	cartData = perishableData;
+        } else {
+        	cartData = nonPerishableData;
+        }
+        console.log(cartData[cartId].productname);
+        var item = cartData[cartId].productname;
+        var price = cartData[cartId].price;
+        var existvalue = $('[data-order-count]').text();
+        $('[data-order-count]').text(parseInt(existvalue)+1);
+        var itemExistVal = checkItemExist(item);
+        if(itemExistVal == 'notfound'){
+        	var qty = 1;
+        } else {
+        	var qty = parseInt(itemArrObj[itemExistVal].qty + 1);
+        }
+        addToCart(item, qty , price);
+    });
+
+    // language selection
+    $(document).off('change', '#selectlang');
+    $(document).on('change', '#selectlang', function(){
+        pageRedirect($(this).val());
+    });
+    
+    //When tile click modal get shown with dynamic details for perishable
+    $(document).off('click', '[data-sub-tile]')
+    $(document).on('click', '[data-sub-tile]', function(){
+    	var itemId = $(this).attr('data-sub-tile');
+    	console.log("Perishable Data - ", perishableData[itemId].productname);
+    	var modal = $('#infomodal');
+
+    	modal.find("[data-prasad-name]").text(perishableData[itemId].productname);
+    	modal.find("[data-prasad-desc]").text(perishableData[itemId].productdesc);
+    	modal.find("[data-prasad-img]").attr('src', perishableData[itemId].imagename);
+    	modal.find("[data-prasad-price]").text(perishableData[itemId].price);
+    	modal.modal();
+    });
+
+    //When tile click modal get shown with dynamic details for non perishable
+    $(document).off('click', '[data-sub-tile-non]')
+    $(document).on('click', '[data-sub-tile-non]', function(){
+    	var itemId = $(this).attr('data-sub-tile-non');
+    	console.log("Non Perishable Data - ", nonPerishableData[itemId].productname);
+    	var modal = $('#infomodal');
+
+    	modal.find("[data-prasad-name]").text(nonPerishableData[itemId].productname);
+    	modal.find("[data-prasad-desc]").text(nonPerishableData[itemId].productdesc);
+    	modal.find("[data-prasad-img]").attr('src', nonPerishableData[itemId].imagename);
+    	modal.find("[data-prasad-price]").text(nonPerishableData[itemId].price);
+    	modal.modal();
+    });
+
+    //when add to cart icon click from modal this function get called
+    $(document).off('click', '[data-custom-cart]');
+    $(document).on('click', '[data-custom-cart]', function(){
+    	var modal = $('#infomodal');
+    	var table = $('[data-order-list]');
+    	var qty = parseInt(modal.find('#qty').val());
+    	var existvalue = $('[data-order-count]').text();
+    	var item = modal.find("[data-prasad-name]").text();
+    	var price = modal.find("[data-prasad-price]").text();
+    	$('[data-order-count]').text(parseInt(existvalue)+parseInt(qty));
+    	var itemExistVal = checkItemExist(item);
+        if(itemExistVal == 'notfound'){
+        	var qty = qty;
+        } else {
+        	var qty = parseInt(itemArrObj[itemExistVal].qty + qty);
+        }
+        addToCart(item, qty , price);
+        modal.modal('hide');
+        $("select").each(function() { this.selectedIndex = 0 });
+    });
+
+    //when cart icon click this function get called
+    $(document).off('click', '[data-cart]')
+    $(document).on('click', '[data-cart]', function(){
+    	var table = $('[data-order-list]');
+    	table.find('tbody').empty();
+    	var totalPrice = 0;
+    	for(var i=0; i<itemArrObj.length; i++){
+        	totalPrice += itemArrObj[i].price;
+    		table.find('tbody').append('<tr><td>'+ itemArrObj[i].name +'</td><td>'+ itemArrObj[i].qty +'</td></tr>')
+    	}
+        $('[data-total-order]').text(totalPrice);
+    });
+
+    $("#Pooja").on('change', function(){
+        var tempval = $(this).val();
+        switch(tempval){
+            case 'Puja 1':
+                $('.pujatab a.pu1').removeClass('hide');
+                $('.pujatab a.pu2').addClass('hide');
+                break;
+            case 'Puja 2':
+                $('.pujatab a.pu2').removeClass('hide');
+                $('.pujatab a.pu1').addClass('hide');
+                break;
+            case 'Puja 3':
+                $('.pujatab a.pu1').removeClass('hide');
+                $('.pujatab a.pu2').addClass('hide');
+                break;
+            case 'Puja 4':
+                $('.pujatab a.pu2').removeClass('hide');
+                $('.pujatab a.pu1').addClass('hide');
+                break;
+            case 'Puja 5':
+                $('.pujatab a.pu1').removeClass('hide');
+                $('.pujatab a.pu2').addClass('hide');
+                break;
+            default:
+                $('.pujatab a.pu2').removeClass('hide');
+                $('.pujatab a.pu1').addClass('hide');
+                break;
+        }
+    });
+    
+});
+
+//reusable for checking item exist
+function checkItemExist(item){
+	if(itemArrObj.length){
+		for(var i=0; i<itemArrObj.length; i++){
+	    	if(itemArrObj[i].name == item){
+	    		return i;
+	    	}
+	    }
+		return 'notfound';
+	} else {
+		return 'notfound';
+	}
+
+}
+
+//function for adding items to cart
+function addToCart(item, qty, price) {
+	var itemObj = {};
+    var itemExistVal = checkItemExist(item);
+
+    if( itemExistVal!= 'notfound'){
+    	itemArrObj[itemExistVal].qty = qty;
+    	itemArrObj[itemExistVal].price = Math.floor(price*qty);
+    } else {
+    	itemObj.name = item;
+        itemObj.qty = qty;
+        itemObj.price = Math.floor(price*qty);
+        itemArrObj.push(itemObj);
+    }
+}
+
+//page redirection based on url
+function pageRedirect(arg){
+    switch(arg){
+        case 'menu':
+            location.href = 'menu.html';
+            break;
+        case 'darshan':
+            location.href = 'darshan.html';
+            break;
+        case 'puja':
+            location.href = 'puja.html';
+            break;
+        case 'English':
+            location.href = "pages/home.html";
+            break;
+        default:
+            location.href = "/";
+            break;
+    }
+}
